19a5f34493299b1879b6f66dcf4f14164eee52e5
EC2 Default User <ec2-user@ip-172-31-21-222.ap-northeast-1.compute.internal>
2015-07-16 06:35:15 +0000

    2015-07-16

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Play::Application.config.secret_key_base = '1903b1b451c6406de2d779b850e50d2a9a12f65162378bbcb19a386bdcdfa88570989ab044178549d19110948da920f6cec207faaea57e2deb8cbf19b7e42743'
